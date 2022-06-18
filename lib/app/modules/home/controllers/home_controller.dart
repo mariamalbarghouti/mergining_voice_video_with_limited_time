0836a91e5c62e7f70228748087e398dbc76d4a28
Mariam Albarghouti <mariamalbarghouti@gmail.com>
2022-06-18 11:04:46 +0200

    add_feature

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,8 +2,10 @@ import 'dart:io';
 
 import 'package:add_voice_over_video_feature/core/print_logger.dart';
 import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter/return_code.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:get/get.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:video_player/video_player.dart';
 
 class HomeController extends GetxController {
@@ -12,44 +14,49 @@ class HomeController extends GetxController {
   String? videoUrlName;
   String? audioUrlName;
   String? outputUrlName;
-  // final String outputUrl =
-  final String outputUrl =
-      // '/storage/emulated/0/Documents/output.mp4';
-      '/data/user/0/com.example.add_voice_over_video_feature/cache/file_picker/dss.mp4';
   VideoPlayerController? videoController;
-
-  // final FlutterFFmpeg _flutterFFmpeg = FlutterFFmpeg();
+  String? outputUrl;
 
   @override
-  void onInit() {
+  Future<void> onInit() async {
     super.onInit();
+    await getDirrPlae();
+    // await storagePermissionRequest();
+  }
+
+  Future<void> getDirrPlae() async {
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    outputUrl = "${appDocDir.path}/mariam.mp4";
+    coloredPrint(msg: "OutputPath $outputUrl");
   }
 
   Future<void> pickAudio() async {
     FilePickerResult? audioResult = await FilePicker.platform.pickFiles(
-      type: FileType.audio,
-      // type: FileType.custom,
-      // allowedExtensions: ['aac','mp3'],
+      // type: FileType.audio,
+      type: FileType.custom,
+      allowedExtensions: ['mp3', 'ogg', 'wav'],
     );
+    coloredPrint(msg: "audio ${audioResult?.files.single.path}");
+    coloredPrint(msg: "audioResult $audioResult");
+
     if (audioResult != null) {
       audioFile = File(audioResult.files.single.path ?? "");
-
       coloredPrint(msg: "file $audioFile");
       audioUrlName = audioFile.path;
       coloredPrint(msg: "file $audioUrlName");
       update();
     } else {
       // User canceled the picker
-
     }
   }
 
   Future<void> pickVideo() async {
     FilePickerResult? videoResult = await FilePicker.platform.pickFiles(
-      type: FileType.video,
-      // type: FileType.custom,
-      // allowedExtensions: ['mp4'],
+      // type: FileType.video,
+      type: FileType.custom,
+      allowedExtensions: ['mp4'],
     );
+    coloredPrint(msg: "Path ${videoResult?.files.single.path}");
     if (videoResult != null) {
       videoFile = File(videoResult.files.single.path ?? "");
       videoUrlName = videoFile.path;
@@ -75,18 +82,21 @@ class HomeController extends GetxController {
     String mergeAudioWithVideoCommand =
         '-i $audioUrlName -i $videoUrlName -c copy $outputUrl';
 
-    String trimOnlyTheFirst30Sec = '-i $videoUrlName -t 30 -c copy $outputUrl';
-
-    FFmpegKit.execute(trimOnlyTheFirst30Sec).then((value) async {
-      FFmpegKit.execute(commandToExecute).then((value) {});
-
-      outputUrlName = outputUrl;
-      // make the music only for image
-      videoController = VideoPlayerController.file(File(outputUrlName!))
-        ..initialize().then((val) {});
-      update();
-      coloredPrint(msg: "audioUrlName:$audioUrlName");
-      coloredPrint(msg: "value: ${await value.getDuration()}");
-    });
+    String trimOnlyTheFirst30Sec = '-i $videoUrlName -t 5 -c copy $outputUrl';
+    // first 30 sec
+    //
+    // Future.wait([
+    await FFmpegKit.execute(commandToExecute);
+   await FFmpegKit.execute(trimOnlyTheFirst30Sec);
+   
+ 
+    // ]).then((value){
+    // coloredPrint(msg: "value: ${await value.getDuration()}");
+    outputUrlName = outputUrl;
+    // make the music only for image
+    videoController = VideoPlayerController.file(File(outputUrlName!))
+      ..initialize().then((val) {});
+    update();
+    coloredPrint(msg: "audioUrlName:$audioUrlName");
   }
 }
